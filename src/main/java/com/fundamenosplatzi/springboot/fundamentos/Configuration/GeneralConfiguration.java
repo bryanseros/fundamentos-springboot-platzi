6fc1b2c1ea0533388155a4b259867dce0b983c37
Bryan Serna Osorio <bryanseros@gmail.com>
2021-08-28 17:48:37 -0500

    Uso de connection.properties para conectar a h2

--- src/main/java/com/fundamenosplatzi/springboot/fundamentos/Configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamenosplatzi/springboot/fundamentos/Configuration/GeneralConfiguration.java
@@ -8,9 +8,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
-
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 @Configuration
 public class GeneralConfiguration {
@@ -23,6 +24,18 @@ public class GeneralConfiguration {
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplements(name, apellido);
@@ -31,10 +44,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("sa");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
